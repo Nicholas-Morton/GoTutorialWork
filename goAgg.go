9f4951d8d9f207ba94c98cb9108b779d0980551a
Nicholas Morton <NMorton@Hunter.com>
2019-01-22 16:57:20 -0600

    Updated to pull more info from the xml; does not function becuase newlines are sprinkled around

--- goAgg.go
+++ goAgg.go
@@ -1,29 +1,62 @@
-package main 
+package main
 
-import ("fmt"
-		"net/http"
-		"io/ioutil"
-		"encoding/xml")
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 type SitemapIndex struct {
-	Locations []Location `xml:"sitemap"`
+	Locations []string `xml:"sitemap>loc"`
 }
 
-type Location struct {
-	Loc string `xml:"loc"`
+type News struct {
+	Titles    []string `xml:"url>news>title"`
+	Keywords  []string `xml:"url>news>keywords`
+	Locations []string `xml:"url>loc"`
 }
 
-func (_l Location) String() string {
-	return fmt.Sprintf(_l.Loc)
+type NewsMap struct {
+	Keyword  string
+	Location string
 }
 
 func main() {
-	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
-	bytes, _ := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
 
 	var s SitemapIndex
+	var n News
+	//NM := make(map[string]NewsMap)
+	i := 0
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
 	xml.Unmarshal(bytes, &s)
 
-	fmt.Println(s.Locations)
+	//s.Locatoins[:1] to remove newlines cause what the fuck
+
+	for _, Location := range s.Locations {
+		resp, _ = http.Get(Location)
+		bytes, _ = ioutil.ReadAll(resp.Body)
+		xml.Unmarshal(bytes, &n)
+		fmt.Printf(n.Titles[i], n.Keywords[i], n.Locations[i])
+		i++
+		/*
+			for idx, _ := range n.Titles {
+				NM[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+				fmt.Println()
+			}
+		*/
+	}
+
+	fmt.Println(len(n.Titles))
+
+	//fmt.Printf("Title: %S Keywords: %S Locations: %S", n.Titles[1], n.Keywords[1], n.Locations[1])
+
+	/*
+		for idx, data := range NM {
+			fmt.Println("\n\n\n", idx)
+			fmt.Println("\n", data.Keyword)
+			fmt.Println("\n", data.Location)
+		}
+	*/
 }
